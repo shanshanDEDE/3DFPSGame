72e4f97493c2cc63bb9b02e88a64c81cea578db4
willy <willyjen1111@gmail.com>
2024-05-09 15:28:11 +0800

    觸發事件 並且有屏障 完成(注意事項:instantiatebarrier1 = Instantiate(barrier1);  要這樣寫才可以正確地知道新生成指定的屏障是哪個)

---
+++ Assets/Scripts/Combat/BossSpawnManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ddc76c096215a05e8519fbf9296d78190fc1a215a253474daac7cc4a89e32cba
+size 1153
--- Assets/Scripts/Combat/EnemySpawnManager.cs
+++ Assets/Scripts/Combat/EnemySpawnManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13edbdf12a8fdad773cdff8df794c96aa739db375a7f6f0dad5b20d2e59ba0f4
-size 2291
+oid sha256:696f555017c1b155a1b892219733fa8818585cda2cf4cb31cad499e046d66978
+size 2965
