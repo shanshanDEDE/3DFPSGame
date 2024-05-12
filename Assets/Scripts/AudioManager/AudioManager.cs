a593a638a3c73bd86fe8c743656172bfc14c72f2
willy <willyjen1111@gmail.com>
2024-05-12 21:20:30 +0800

    音效介面及關掉遊戲後會儲存音樂資訊

--- Assets/Scripts/AudioManager/AudioManager.cs
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:879b2a18b88b58e864a551038b9ed403c53aa21a8ef666423c1fe0ad10a7653a
-size 3092
+oid sha256:7b9e9050dd012d3b7a17cc5fc93e039fdcd24fb1b332501d096482c7b6adeb49
+size 4070
---
+++ Assets/Scripts/AudioManager/VolumeSetting.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4c5fe21bc14a233b76f4b44d331c13c18a4ee0eb447c7b3004136b731419282
+size 1138
