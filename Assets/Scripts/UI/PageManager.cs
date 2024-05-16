3aa665ae397362aff0767cc2ba6ebd65ba10bc58
willy <willyjen1111@gmail.com>
2024-05-16 22:54:18 +0800

    做了一點遊戲操作說明

--- Assets/Scripts/UI/PageManager.cs
+++ Assets/Scripts/UI/PageManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bc1522917f1a69e84ce80cd11b9486ba5d3d0f2450bca4d5ba00c40089d73e2
-size 1746
+oid sha256:a4cde0d7af8f4205265587c876fee975387b8bc212103a71919a46d0c37e2aa6
+size 1854
