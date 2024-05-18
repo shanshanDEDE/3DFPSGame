d57d9b3b177289f4cf66d8bed71e8982370e1edf
willy <willyjen1111@gmail.com>
2024-05-18 14:32:50 +0800

    新特效asset05

---
+++ Assets/JMO Assets/Cartoon FX Remaster/CFXR Assets/Editor/CFXR_ShaderImporter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6cb875e71845956e45d15969d1c7e45a0fb427e65912bfe0d24f344cf998a06c
+size 16135
---
+++ Assets/JMO Assets/Cartoon FX Remaster/Readme Cartoon FX Remaster FREE.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f222d1b530ef5b2b4b1ca4a89c768fa7de8d1453fab528986b651d660efbb663
+size 5448
