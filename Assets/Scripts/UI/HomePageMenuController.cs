30d7d04633e4c589f3eb281597e6927626a64a7a
willy <willyjen1111@gmail.com>
2024-05-18 10:38:17 +0800

    準備設計第二關

--- Assets/Scripts/UI/HomePageMenuController.cs
+++ Assets/Scripts/UI/HomePageMenuController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bee2da448b872f0106b3ccc019821edde5dfe8727894f53b8bf977ff17203018
-size 1760
+oid sha256:87ec271c0de2d9a492e406491f7a3a47ecca3e52dacd0eadb742dc4c26f26c06
+size 2519
