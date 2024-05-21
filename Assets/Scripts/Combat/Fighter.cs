0eb186fe657a98abd23e91d06ff901eb269105a8
willy <willyjen1111@gmail.com>
2024-05-21 13:37:31 +0800

    骷髏大劍能量攻擊

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fff2b1adc5e8d38f168d7bb7c639be9e3cdb044220ae911788afb2afd45a3f2
-size 14280
+oid sha256:abc8f8e80488c8cd0b350df6781c8c00ecc80a2c300825c5ae532e93390e64d9
+size 15711
--- Assets/Scripts/control/AIController.cs
+++ Assets/Scripts/control/AIController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c70a7682aee9cfb414a131fd8467db010591277fbc895bc9a16d9ed64cb109
-size 4846
+oid sha256:119dd3969c561a0f090fc732339daa9186159dca91d9e57dff43000462b453df
+size 5119
