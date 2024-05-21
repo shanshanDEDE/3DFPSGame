880eea08499d61e014bec200c8ec813f07542a80
willy <willyjen1111@gmail.com>
2024-05-21 23:00:06 +0800

    發射成功!!!

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c761337db13d8b7940b949f7e8585e7b3403b76ad161abb2f37df27e72e6fe4
-size 21310
+oid sha256:367eb89f1c2ee646bbc8ddc4ddd37535532070e9ed62ac3a9bae4ed51c20a5d2
+size 21269
